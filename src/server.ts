39b68dc5b21dbd429f8eba74952bb027f48bdf56
juliasfrohlich <juliasfrohlich@hotmail.com>
2021-06-21 13:32:06 -0300

    feat(NLW 06): Adiciona typescript e primeiras rotas ao projeto

--- src/server.ts
+++ src/server.ts
@@ -4,3 +4,11 @@ const app = express()
 
 // http://localhost:3000
 app.listen(3000, () => console.log("Server is running."))
+
+app.get("/test", (request, response) => {
+  return response.send("Olá NLW")
+})
+
+app.post("/test-post", (request, response) => {
+  return response.send("Olá NLW Método Post")
+})
