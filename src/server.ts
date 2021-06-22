bf47214bab1cae72ad45e1c5884e155ebb666d93
juliasfrohlich <juliasfrohlich@hotmail.com>
2021-06-22 14:30:59 -0300

    feat(NLW 6): Adiciona database ao projeto

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,7 @@
+import "reflect-metadata";
 import express from "express";
+import "./db";
 
 const app = express()
 
-// http://localhost:3000
 app.listen(3000, () => console.log("Server is running."))
-
-app.get("/test", (request, response) => {
-  return response.send("Olá NLW")
-})
-
-app.post("/test-post", (request, response) => {
-  return response.send("Olá NLW Método Post")
-})
