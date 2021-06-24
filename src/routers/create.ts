ef96a2b90179f6989c4b5667ccf3e166e12028d9
juliasfrohlich <juliasfrohlich@hotmail.com>
2021-06-24 17:27:51 -0300

    feat(NLW 6): Cria a entidade "Compliment" e seus respectivos service, controller e repository

--- src/routers/create.ts
+++ src/routers/create.ts
@@ -1,13 +1,16 @@
 import { Router } from "express";
 import { createTag as CreateTagController } from "../controllers/createTag"
 import { createUser as CreateUserController } from "../controllers/createUser"
+import { createCompliment as CreateComplimentController } from "../controllers/createCompliment"
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 
 const router = Router();
 const createTagController = new CreateTagController()
 const createUserController = new CreateUserController()
+const createComplimentController = new CreateComplimentController()
 
 router.post("/tags", ensureAdmin, createTagController.handle)
 router.post("/users", createUserController.handle)
+router.post("/compliments", createComplimentController.handle)
 
 export { router }
