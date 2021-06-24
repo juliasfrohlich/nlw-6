878a56982ce9e528b1a56db317219fe181fd7c23
juliasfrohlich <juliasfrohlich@hotmail.com>
2021-06-23 23:23:02 -0300

    feat(NLW 6): Cria middleware que permite que apenas usuários administradores inseram tags

--- src/routers/create.ts
+++ src/routers/create.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { createTag as CreateTagController } from "../controllers/createTag"
-import {createUser as CreateUserController} from "../controllers/createUser"
+import { createUser as CreateUserController } from "../controllers/createUser"
+import { ensureAdmin } from "../middlewares/ensureAdmin";
 
 const router = Router();
 const createTagController = new CreateTagController()
 const createUserController = new CreateUserController()
 
-router.post("/tags", createTagController.handle)
+router.post("/tags", ensureAdmin, createTagController.handle)
 router.post("/users", createUserController.handle)
-
 
 export { router }
